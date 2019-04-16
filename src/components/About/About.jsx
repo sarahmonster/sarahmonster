7715505589c4e7fc54ba27382837ac3b2865c517
sarah <sarah@triggersandsparks.com>
2019-04-16 23:11:34 +0100

    Implement a basic and medium-sassy about page, using Nautilus components.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import "./About.css";
+import { Paragraph, Button, Heading } from 'nautilus-system/src';
 
 class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>
-          Edit About component or pages/about.jsx to include your information.
-        </h1>
+        <Heading level={1} size="xxlarge">Hello there! I'm Sarah and  there's a 98% chance I don't much care for you.</Heading>
+		<Paragraph>I’m primarily a visual and product designer, but I have a strong technical background and a wide-ranging general knowledge that allows me to approach my work in a holistic way. I care deeply about diversity and inclusivity in tech and frequently speak at events across the world. My career has been marked by a continued process of experimentation and innovation, bringing new ideas to every project I join.</Paragraph>
+		<Paragraph>Let’s make great things together!</Paragraph>
+		<Button>Hello, there!</Button>
       </div>
     );
   }
