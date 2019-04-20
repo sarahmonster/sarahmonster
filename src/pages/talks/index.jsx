b0e90e9a5184646fa26dd87fc45629282b9f7b2b
sarah <sarah@triggersandsparks.com>
2019-04-20 23:58:02 +0100

    Add a listing of speaking engagements to talks page.

--- src/pages/talks/index.jsx
+++ src/pages/talks/index.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
+import { Heading, Paragraph } from "nautilus-system/src";
+
 import Layout from "../../layout";
 import PostListing from "../../components/post-listing";
+import SpeakingEngagementList from "../../components/SpeakingEngagementList";
 import SEO from "../../components/SEO";
 import config from "../../../data/site-config";
 
 class Index extends React.Component {
   render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
+    const allTalks = this.props.data.allMarkdownRemark.edges;
+    const allEvents = this.props.data.allSpeakingEngagementsJson.edges;
+
     return (
       <Layout>
-        <div className="index-container">
-          <Helmet title={config.siteTitle} />
-          <SEO />
-          <PostListing postEdges={postEdges} />
-        </div>
+        <Helmet title={config.siteTitle} />
+        <SEO />
+
+        <Heading size="xxlarge" level={1}>Speaking</Heading>
+        <Heading size="medium" level={0}>Sometimes I do it.</Heading>
+
+        <Paragraph>No lie, I'm sometimes terrified of public speaking. But I think it's good to do things that terrify you, and I like sharing my ideas, so I do it...pretty frequently.</Paragraph>
+
+        <Heading size="large" level={2}>Speaking engagements</Heading>
+        <SpeakingEngagementList postEdges={allEvents} />
+
+        <Heading size="large" level={2}>Things I've talked about</Heading>
+        <PostListing postEdges={allTalks} />
       </Layout>
     );
   }
@@ -25,7 +38,19 @@ export default Index;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
-  query GetAllTalks {
+  query getAllTalks {
+    allSpeakingEngagementsJson {
+      edges {
+        node {
+          id
+          title
+          date
+          url
+          city
+          countryCode
+        }
+      }
+    }
     allMarkdownRemark(
       filter: { fileAbsolutePath: {regex : "\/talks/"} },
       limit: 2000
@@ -50,3 +75,5 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+
